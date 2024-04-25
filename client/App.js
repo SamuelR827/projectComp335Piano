542db12f3417669a97bb42dae5eedd1b65d670d8
Amber Chaves <a1chaves@student.bridgew.edu>
2024-04-25 15:00:53 -0400

    testing to see if i have to redo it

--- client/App.js
+++ client/App.js
@@ -3,7 +3,7 @@ import Piano from './Piano'; // import piano file
 
 function App() {
   // Define keys list
-  const keys = ['C', 'C#\nDd', 'D', 'D#\nEd', 'E', 'F', 'F#\nGd', 'G', 'G#\nAd', 'A', 'A#\nBd', 'B', 'C', 'C#\nDd', 'D', 'D#\nEd', 'E', 'F', 'F#\nGd', 'G', 'G#\nAd', 'A', 'A#\nBd', 'B']; // example keys to be changed/added
+  const keys = ['C test', 'C#\nDd', 'D', 'D#\nEd', 'E', 'F', 'F#\nGd', 'G', 'G#\nAd', 'A', 'A#\nBd', 'B', 'C', 'C#\nDd', 'D', 'D#\nEd', 'E', 'F', 'F#\nGd', 'G', 'G#\nAd', 'A', 'A#\nBd', 'B']; // example keys to be changed/added
 
   return (
     <div className="App">
