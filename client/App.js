02c90be7459906ea6906a02c4f385650d1509515
Amber Chaves <a1chaves@student.bridgew.edu>
2024-04-29 15:25:18 -0400

    add table and put letters on keyboard

--- client/App.js
+++ client/App.js
@@ -3,7 +3,7 @@ import Piano from './Piano'; // import piano file
 
 function App() {
   // Define keys list
-  const keys = ['C', 'C#\nDd', 'D', 'D#\nEd', 'E', 'F', 'F#\nGd', 'G', 'G#\nAd', 'A', 'A#\nBd', 'B', 'C', 'C#\nDd', 'D', 'D#\nEd', 'E', 'F', 'F#\nGd', 'G', 'G#\nAd', 'A', 'A#\nBd', 'B'];
+  const keys = ['Q', '2', 'W', '3', 'E', 'R', '5', 'T', '6', 'Y', '7', 'U', 'I', '9', 'O', '0', 'P', '[', '=', ']', 'A', 'Z', 'S', 'X'];
   return (
     <div className="App">
       {/* Render the Piano component with the keys data */}
